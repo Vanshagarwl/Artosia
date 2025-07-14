59c1bf8289fccd47ac44766b9c60a139997dd406
Vanshagarwl <agarwalvansh788@gmail.com>
2025-07-15 00:30:57 +0530

    Add Painting model with schema definition for MongoDB

---
+++ server/models/Painting.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const paintingSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  price: { type: Number, required: true },
+  paperType: { type: String, required: true },
+  description: { type: String, required: true },
+  size: { type: String, required: true },
+  artistName: { type: String, required: true },
+  category: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Painting', paintingSchema);
