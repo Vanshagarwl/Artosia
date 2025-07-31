72ea6b623f4deea23edc53bbd94c6c700cb2adc0
Vanshagarwl <agarwalvansh788@gmail.com>
2025-07-31 20:34:20 +0530

    Add morgan middleware for logging HTTP requests

--- server/index.js
+++ server/index.js
@@ -4,10 +4,12 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const paintingRoutes = require('./routes/paintingRoutes');
 const os = require('os');
+const morgan = require('morgan');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'));
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
