cf135a06eede43d651a9cc6f2c12af7f8327f39c
Vanshagarwl <agarwalvansh788@gmail.com>
2025-07-31 20:48:05 +0530

    Add deletePainting route

--- server/routes/paintingRoutes.js
+++ server/routes/paintingRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { addPainting, getPaintings } = require('../controllers/paintingController');
+const { addPainting, getPaintings, deletePainting } = require('../controllers/paintingController');
 
 router.post('/', addPainting);
-
 router.get('/', getPaintings);
+router.delete('/:id', deletePainting);
 
 module.exports = router;
