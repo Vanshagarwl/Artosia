8faab67ccabcb85f63289b3f667056e0aeac63b2
Vanshagarwl <agarwalvansh788@gmail.com>
2025-08-01 22:08:58 +0530

    Add authentication middleware to painting creation route

--- server/routes/paintingRoutes.js
+++ server/routes/paintingRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { addPainting, getPaintings, deletePainting,updatePainting } = require('../controllers/paintingController');
+const { addPainting, getPaintings, deletePainting, updatePainting } = require('../controllers/paintingController');
+const auth = require('../middleware/auth');
 
-router.post('/', addPainting);
+router.post('/', auth, addPainting);
 router.get('/', getPaintings);
 router.delete('/:id', deletePainting);
 router.put('/:id', updatePainting);
